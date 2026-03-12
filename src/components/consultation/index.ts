67230ade9769e089f685f5d94e0650be242de487
keylinkcorp <marketing@keylinkbh.com>
2026-03-12 15:04:48 +0300

    Revert "keylink-corp-website-v1 into v2"
    This reverts commit 817971c82b1aff8fbc8cd4473868d512573093fb.

--- src/components/consultation/index.ts
+++
@@ -1,5 +0,0 @@
-export { ConsultationHero } from "./ConsultationHero";
-export { CalendlyBooking } from "./CalendlyBooking";
-export { ConsultationProcess } from "./ConsultationProcess";
-export { ConsultationFAQ } from "./ConsultationFAQ";
-export { ConsultationCTA } from "./ConsultationCTA";
--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-// GitHub sync test - 2026-02-03 - Sync verification complete
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
